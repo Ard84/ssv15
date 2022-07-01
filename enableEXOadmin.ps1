94cd2ee451ddf3961a6f65434c443619a3e2ed38
Ard <86302561+Ard84@users.noreply.github.com>
2022-07-01 14:45:03 +0200

    Add files via upload

---
+++ enableEXOadmin.ps1
@@ -0,0 +1,18 @@
+﻿Connect-AzureAD
+$AutomationAccountName = 'securescoreaut1'
+$ResourceGroup = 'atsecurescorenewversion2v12'
+$AutomationAccount = Get-AzAutomationAccount -Name $AutomationAccountName -ResourceGroupName $ResourceGroup;   
+
+Write-Host '***** Assing Exchange Role for SP *****';
+                        Write-Host '***** Role Exchange Administrator not enabled. Enabling now *****';
+                        $RoleTemplate = Get-AzureADDirectoryRoleTemplate | where {$_.displayName -eq 'Exchange Administrator'};
+                        Write-Host '----- Exchange Administrator Role Template -----';
+                        $RoleTemplate;
+                        Write-Host '----- Enabling Exchange Administrator role -----';
+                        Enable-AzureADDirectoryRole -RoleTemplateId $RoleTemplate.ObjectId;
+                        $AADRole = Get-AzureADDirectoryRole -Filter "DisplayName eq 'Exchange Administrator'";
+                        Write-Host '----- AADRole -----';
+                        $AADRole;
+
+                    Add-AzureADDirectoryRoleMember -ObjectId $AADRole.ObjectId -RefObjectId $AutomationAccount.Identity.PrincipalId;
+                    Write-Host '***** Done *****';
